302364ac91cda2d8b259bf0feac1e2bac608b244
Trevor Coleman <trevor@trevorcoleman.design>
2020-05-18 16:44:50 -0400

    Remove Container from App.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,18 @@
-import Container from '@material-ui/core/Container';
 import React from 'react';
-import ProjectList from './Components/ProjectList/';
 import NavAndSidebar from './Layout/NavAndSidebar';
 import ProjectsRoute from './Routes/Projects/ProjectsRoute';
 import ProjectInspector from './Views/ProjectInspector';
-import logo from './logo.svg';
 import './App.css';
-import { makeStyles, ThemeProvider } from '@material-ui/styles';
-
+import { ThemeProvider } from '@material-ui/styles';
 import theme from './Theme'
 
-const useStyles = makeStyles({
-    background: {
-        backgroundColor: '#eee'
-    }
-});
-
 function App() {
-    const classes = useStyles()
   return (
     <div><ThemeProvider theme={theme}>
         <NavAndSidebar>
-            <ProjectsRoute><Container>
+            <ProjectsRoute>
                 <ProjectInspector />
-            </Container></ProjectsRoute>
+            </ProjectsRoute>
         </NavAndSidebar>
     </ThemeProvider></div>
   );
